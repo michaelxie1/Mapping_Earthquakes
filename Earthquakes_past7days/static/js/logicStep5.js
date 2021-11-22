2945fe9f756794f17e74ff869163d291fc67ab36
michaelxie1 <60826549+michaelxie1@users.noreply.github.com>
2021-11-22 10:52:51 -0500

    Created the earthquake challenge

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -7,9 +7,11 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      accessToken: API_KEY
+    maxZoom: 18,
+    accessToken: API_KEY
   });
+
+
 
 // Create a base layer that holds both maps.
 let baseMaps = {
